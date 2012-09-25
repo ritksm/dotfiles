977016b9ad057add998671de9b760bb988b5baf1
Jack River <ritksm@gmail.com>
2012-09-26 02:29:13 +0800

    changed vim backup setting and updated bootstrap.sh

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ dotfiles
 ========
 
 my dotfiles for daily use
+
+maybe dangerous for you, use it at your own risk
+
+run bootstrap.sh in the project directory to initialize
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,12 @@ unlink ~/.vim/bundles.vim
 unlink ~/.vim/bundle
 unlink ~/.gitconfig
 
+# link git config
 ln -s $current_dir/git/gitconfig ~/.gitconfig
+
+# link vim config
 ln -s $current_dir/vim/vimrc ~/.vimrc
 mkdir ~/.vim/ -p
 ln -s $current_dir/vim/bundles.vim ~/.vim/bundles.vim
 ln -s $current_dir/vim/bundle ~/.vim/bundle
+mkdir ~/.tmp/ -p # make tmp directory for vim temp files
